12cc4438ca49e956d12c03d5e392cac6026ab01d
Ph.D Nurzhanov Chingiz <nurzhanovchingiz89@gmail.com>
2025-11-01 18:17:15 +0500

    Add param for choise prepare data or not

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 class CFG:
     DATASET = "animatronbot/mnist-digit-recognizer"
+    DATA_PREPARATION = True
     SEED = 42
     TEST_SIZE = 0.2
     DOWNLOAD_PATH = "mnist_data"
--- run_common_alg_cv.py
+++ run_common_alg_cv.py
@@ -2,11 +2,15 @@ from common_alg_cv.mnist_download import KaggleDatasetDownloader, make_images_fr
 from common_alg_cv.prepare_mnist_dataset import prepare_mnist_dataset
 from config import CFG
 import time 
-if __name__ == "__main__":
-    start_time = time.time()
+
+def run_data_preparation():
     mnist_downloader = KaggleDatasetDownloader(dataset_id=CFG.DATASET)
     mnist_downloader.run()
     make_images_from_csv()
     prepare_mnist_dataset()
+if __name__ == "__main__":
+    start_time = time.time()
+    if CFG.DATA_PREPARATION:
+        run_data_preparation()
     end_time = time.time()
     print(f"Total time taken: {end_time - start_time} seconds")

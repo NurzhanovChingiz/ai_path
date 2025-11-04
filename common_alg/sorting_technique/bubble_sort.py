610d724f9460719cd804717b95aa033750184e8b
Ph.D Nurzhanov Chingiz <nurzhanovchingiz89@gmail.com>
2025-11-05 03:19:43 +0500

    Add Bubble sort, Inserction sort, Merge sort, Quick sort

---
+++ common_alg/sorting_technique/bubble_sort.py
@@ -0,0 +1,17 @@
+# Bubble Sort
+
+from typing import List, Generator
+
+def bubble_sort(a: List[int]) -> Generator[List[int], None, None]:
+    '''
+    Bubble Sort Algorithm
+    :param a: List of integers to be sorted
+    :yield: The list after each swap operation
+    '''
+    arr = a
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - 1 - i):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+            yield arr
---
+++ common_alg/sorting_technique/insertion_sort.py
@@ -0,0 +1,15 @@
+# Insertion Sort
+
+from typing import List, Generator
+
+def insertion_sort(a: List[int]) -> Generator[List[int], None, None]:
+    arr = a
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j+1] = arr[j]
+            j -= 1
+            yield arr
+        arr[j+1] = key
+        yield arr

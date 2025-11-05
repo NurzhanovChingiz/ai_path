16c71c7f4419ab5fb66b6dd80990fca13b8f249b
Ph.D Nurzhanov Chingiz <nurzhanovchingiz89@gmail.com>
2025-11-05 23:18:41 +0500

    Add Minkowski distance

--- common_alg/distance/euclidean.py
+++ common_alg/distance/euclidean.py
@@ -3,5 +3,12 @@
 import numpy as np
 
 def euclidean(d1: np.ndarray, d2: np.ndarray) -> float:
-        return np.sqrt(np.sum((d1-d2)**2))
+    '''Calculate the Euclidean distance between two points.
+    args:
+        d1: First point as a numpy array.
+        d2: Second point as a numpy array.
+    returns:
+        The Euclidean distance as a float.
+    '''
+    return np.sqrt(np.sum((d1-d2)**2))
     
--- common_alg/distance/main.py
+++ common_alg/distance/main.py
@@ -1,9 +1,12 @@
 import numpy as np
 from euclidean import euclidean
 from manhattan import manhattan
+from minkowski import minkowski
 
 if __name__ == "__main__":
     d1 = np.array([1, 2, 3])
     d2 = np.array([4, 5, 6])
-    print(euclidean(d1, d2))
-    print(manhattan(d1, d2))
+    
+    print('Euclidean distance:',euclidean(d1, d2))
+    print('Manhattan distance:',manhattan(d1, d2))
+    print('Minkowski distance:',minkowski(d1, d2, 2))

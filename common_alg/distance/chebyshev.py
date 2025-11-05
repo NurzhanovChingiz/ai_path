a6f270897a304b50e994bb7b37b639c7cc50e60c
Ph.D Nurzhanov Chingiz <nurzhanovchingiz89@gmail.com>
2025-11-05 23:24:23 +0500

    Add Chebyshev distance

---
+++ common_alg/distance/chebyshev.py
@@ -0,0 +1,12 @@
+# Chebyshev distance or L∞ norm
+import numpy as np
+
+def chebyshev(d1: np.ndarray, d2: np.ndarray) -> float:
+    '''Calculate the Chebyshev distance or L∞ norm between two points.
+    args:
+        d1: First point as a numpy array.
+        d2: Second point as a numpy array.
+    returns:
+        The Chebyshev distance as a float.
+    '''
+    return np.max(np.abs(d1 - d2))

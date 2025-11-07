aa8a1aa067ca2becace9555def4f69db61a2f13c
Ph.D Nurzhanov Chingiz <nurzhanovchingiz89@gmail.com>
2025-11-07 11:18:08 +0500

    Add clear gpu memory

---
+++ common_alg_cv/alg_cv/clear_gpu.py
@@ -0,0 +1,13 @@
+import torch
+import time
+import gc
+
+def clear_memory(verbose: bool = True):
+    stt = time.time()
+    if torch.cuda.is_available():
+        torch.cuda.synchronize()
+        torch.cuda.empty_cache()  # https://forums.fast.ai/t/clearing-gpu-memory-pytorch/14637
+    gc.collect()
+
+    if verbose:
+        print('Cleared memory.  Time taken was %f secs' % (time.time() - stt))

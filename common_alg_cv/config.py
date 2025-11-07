23ccf269bdceb60b66468eead9458e21b7cd6acc
Ph.D Nurzhanov Chingiz <nurzhanovchingiz89@gmail.com>
2025-11-07 11:14:53 +0500

    Add set_seed and val to split

---
+++ common_alg_cv/config.py
@@ -0,0 +1,10 @@
+class CFG:
+    SEED = 42
+    SHOW_IMG = False
+    img_size = 28
+    batch_size = 32
+    epochs = 10
+    learning_rate = 0.001
+    momentum = 0.9
+    num_classes = 10
+    device = 'dml'  # or 'cpu'

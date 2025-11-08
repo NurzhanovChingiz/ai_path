f9313ddde23ee6d224c1df871635091b269eb9db
Ph.D Nurzhanov Chingiz <nurzhanovchingiz89@gmail.com>
2025-11-08 21:11:31 +0500

    Change to config have model, optimizer and loss func. For more confort way to change it

--- common_alg_cv/config.py
+++ common_alg_cv/config.py
@@ -1,12 +1,25 @@
 import torch_directml
+from torch import nn
+import torch
+from models.resnet import ResNet, cfg_resnet, BasicBlock
+from alg_cv.set_seed import set_seed
+from alg_cv.clear_gpu import clear_memory
 class CFG:
     SEED = 42
+    set_seed(SEED)
+    clear_memory()
+    
     SHOW_IMG = False
     IMG_SIZE = 28
     BATCH_SIZE = 64
-    EPOCHS = 10
-    LEARNING_RATE = 0.001
+    EPOCHS = 2
+    LR = 0.001
     WEIGHT_DECAY = 1e-4
     MOMENTUM = 0.9
     NUM_CLASSES = 10
+    
+    # Define the model, loss function, and optimizer
     DEVICE = torch_directml.device(torch_directml.default_device())
+    MODEL = ResNet(BasicBlock, cfg_resnet['ResNet34']).to(DEVICE)
+    LOSS_FN = nn.CrossEntropyLoss()
+    OPTIMIZER = torch.optim.SGD(MODEL.parameters(), lr=LR, momentum=MOMENTUM, weight_decay=WEIGHT_DECAY)

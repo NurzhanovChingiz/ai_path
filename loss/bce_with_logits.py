bfc100d53b44f32c8110969063974bc5909adf59
Nurzhanov Chingiz <nurzhanovchingiz89@gmail.com>
2026-02-18 18:42:27 +0500

    Add bce_with_logits

---
+++ loss/bce_with_logits.py
@@ -0,0 +1,53 @@
+# binary cross entropy with logits
+import numpy as np 
+import torch
+from torch.nn import functional as F
+from torch import nn
+
+def sigmoid_np(x: np.ndarray) -> np.ndarray:
+    result: np.ndarray = 1 / (1 + np.exp(-x))
+    return result
+
+def bce_with_logits_np(y: np.ndarray, y_pred: np.ndarray) -> float:
+    """
+    Numpy implementation of binary cross entropy with logits.
+    y: class indices of shape (N,)
+    y_pred: raw logits of shape (N,)
+    """
+    sigma = sigmoid_np(y_pred)
+    bce: np.ndarray = -1 * (y * np.log(sigma) + (1 - y) * np.log(1 - sigma))
+    result: float = bce.mean()
+    return result
+
+def sigmoid_torch(x: torch.Tensor) -> torch.Tensor:
+    result: torch.Tensor = 1 / (1 + torch.exp(-x))
+    return result
+
+def bce_with_logits_torch(y: torch.Tensor, y_pred: torch.Tensor) -> torch.Tensor:
+    """
+    PyTorch implementation of binary cross entropy with logits.
+    y: class indices of shape (N,)
+    y_pred: raw logits of shape (N,)
+    """
+    sigma = sigmoid_torch(y_pred)
+    bce = -1 * (y * torch.log(sigma) + (1 - y) * torch.log(1 - sigma))
+    result: torch.Tensor = bce.mean()
+    return result
+
+if __name__ == "__main__":
+    input = torch.randn(3, requires_grad=True)
+    target = torch.empty(3).random_(2)
+    torch_loss = F.binary_cross_entropy_with_logits(input, target)
+    torch_loss.backward()
+    print(input)
+    print(target)
+    print(f"PyTorch bce_with_logits: {torch_loss.item():.6f}")
+
+    # Our implementation
+    target_np = target.detach().numpy()
+    input_np = input.detach().numpy()
+    our_np_loss = bce_with_logits_np(target_np, input_np)
+    print(f"Our numpy bce_with_logits:     {our_np_loss:.6f}")
+    our_torch_loss = bce_with_logits_torch(target, input)
+    our_torch_loss.backward()
+    print(f"Our torch bce_with_logits:     {our_torch_loss.item():.6f}")
--- loss/crossentropyloss.py
+++ loss/crossentropyloss.py
@@ -5,24 +5,27 @@ from torch.nn import functional as F
 import numpy as np
 
 # numpy implementation of softmax, log_softmax, cross_entropy
-def softmax_np(logits):
+def softmax_np(logits: np.ndarray) -> np.ndarray:
     m = np.max(logits, axis=1, keepdims=True)
     shifted_logits = logits - m
     exp_i = np.exp(shifted_logits)
     exp_j = np.sum(exp_i, axis=1, keepdims=True)
-    return exp_i / exp_j # softmax_i = exp_i / exp_j
+    result: np.ndarray = exp_i / exp_j # softmax_i = exp_i / exp_j
+    return result
 
-def log_softmax_np_1(logits):
-    return np.log(softmax_np(logits)) # log_softmax_i = log(softmax_i)
+def log_softmax_np_1(logits: np.ndarray) -> np.ndarray:
+    result: np.ndarray = np.log(softmax_np(logits)) # log_softmax_i = log(softmax_i)
+    return result
 
-def log_softmax_np(logits):
+def log_softmax_np(logits: np.ndarray) -> np.ndarray:
     m = np.max(logits, axis=1, keepdims=True)
     shifted_logits = logits - m
     exp_i = np.exp(shifted_logits)
     exp_j = np.sum(exp_i, axis=1, keepdims=True)
-    return shifted_logits - np.log(exp_j) # log_softmax_i = yi - log(exp_j)
+    result: np.ndarray = shifted_logits - np.log(exp_j) # log_softmax_i = yi - log(exp_j)
+    return result
 
-def cross_entropy_np(y, y_pred):
+def cross_entropy_np(y: np.ndarray, y_pred: np.ndarray) -> float:
     """
     Numpy implementation of multi-class cross entropy loss.
     y: class indices of shape (N,)
@@ -32,27 +35,29 @@ def cross_entropy_np(y, y_pred):
     row = np.arange(len(y))
     cols = y
     ce = -1 * log_probs[row, cols]
-    return ce.mean()
+    result: float = ce.mean()
+    return result
+
 
 # pytorch implementation of softmax, log_softmax, cross_entropy
-def softmax_torch(logits):
+def softmax_torch(logits: torch.Tensor) -> torch.Tensor:
     m = logits.max(dim=1, keepdim=True).values # m = max(logits_i)
     shifted_logits = logits - m # shifted_logits_i = logits_i - m
     exp_i = torch.exp(shifted_logits) # exp_i = exp(shifted_logits_i)
     exp_j = torch.sum(exp_i, dim=1, keepdim=True) # exp_j = sum(exp_i)
     return exp_i / exp_j # softmax_i = exp_i / exp_j
 
-def log_softmax_torch_1(logits):
+def log_softmax_torch_1(logits: torch.Tensor) -> torch.Tensor:
     return torch.log(softmax_torch(logits)) # log_softmax_i = log(softmax_i)
 
-def log_softmax_torch(logits):
+def log_softmax_torch(logits: torch.Tensor) -> torch.Tensor:
     m = logits.max(dim=1, keepdim=True).values # m = max(logits_i)
     shifted_logits = logits - m # shifted_logits_i = logits_i - m
     exp_i = torch.exp(shifted_logits) # exp_i = exp(shifted_logits_i)
     exp_j = torch.sum(exp_i, dim=1, keepdim=True) # exp_j = sum(exp_i)
     return shifted_logits - torch.log(exp_j) # log_softmax_i = yi - log(exp_j)
 
-def cross_entropy_torch(y, y_pred):
+def cross_entropy_torch(y: torch.Tensor, y_pred: torch.Tensor) -> torch.Tensor:
     """
     Pytorch implementation of multi-class cross entropy loss.
     y: class indices of shape (N,)
@@ -74,6 +79,7 @@ if __name__ == "__main__":
 
     # PyTorch reference
     torch_loss = F.cross_entropy(input, target)
+    torch_loss.backward()
     print(f"PyTorch cross_entropy: {torch_loss.item():.6f}")
 
     # Our implementation
@@ -83,4 +89,6 @@ if __name__ == "__main__":
     print(f"Our numpy cross_entropy:     {our_np_loss:.6f}")
 
     our_torch_loss = cross_entropy_torch(target, input)
+    our_torch_loss.backward()
     print(f"Our torch cross_entropy:     {our_torch_loss.item():.6f}")
+   

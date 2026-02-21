8af46d6b1f21ad4b5c02c11cceed75af5b415d6b
Nurzhanov Chingiz <nurzhanovchingiz89@gmail.com>
2026-02-21 15:58:12 +0500

    Add detection loss module stub Add comment components of detection loss

---
+++ loss/detection/detection_loss.py
@@ -0,0 +1,7 @@
+# Detection loss
+# Detection loss use a composite loss function that combines multiple loss functions.
+# class classification loss, bounding box regression loss, and objectness loss.
+
+# Class classification loss: CE / BCE / Focal Loss
+# Objectness loss: BCE / Focal Loss
+# Bounding box regression loss: SmoothL1 / L1 / L2 / IoU / GIoU / DIoU / CIoU

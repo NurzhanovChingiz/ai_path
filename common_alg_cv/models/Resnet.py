06a978e107e2cb1c0b06e706df2533d39e82d222
Nurzhanov Chingiz <nurzhanovchingiz89@gmail.com>
2026-02-26 16:33:22 +0500

    Add new implimentation of resnet

--- common_alg_cv/models/Resnet.py
+++ common_alg_cv/models/Resnet.py
@@ -1,28 +1,13 @@
+from typing import Optional, Callable, Union
+from torch import Tensor
+from torch import nn
 import torch
-import torch.nn as nn
 
-cfg_resnet = {
-    'ResNet10': [1, 1, 1, 1],
-    'ResNet14': [1, 2, 2, 1],
-    'ResNet18': [2, 2, 2, 2],
-    'ResNet26': [2, 2, 4, 2],
-    'ResNet34': [3, 4, 6, 3],
-    'ResNet50': [3, 4, 6, 3],
-    'ResNet101': [3, 4, 23, 3],
-    'ResNet152': [3, 8, 36, 3]
-}
-
-
-def conv3x3(
-        in_planes: int,
-        out_planes: int,
-        stride: int = 1,
-        groups: int = 1,
-        dilation: int = 1) -> nn.Conv2d:
+def conv3x3(in_planes: int, out_planes: int, stride: int = 1, groups: int = 1, dilation: int = 1) -> nn.Conv2d:
     """3x3 convolution with padding"""
     return nn.Conv2d(
-        in_channels=in_planes,
-        out_channels=out_planes,
+        in_planes,
+        out_planes,
         kernel_size=3,
         stride=stride,
         padding=dilation,
@@ -32,22 +17,42 @@ def conv3x3(
     )
 
 
+def conv1x1(in_planes: int, out_planes: int, stride: int = 1) -> nn.Conv2d:
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
 class BasicBlock(nn.Module):
     expansion: int = 1
 
-    def __init__(self, inplanes: int, planes: int, stride: int = 1,
-                 downsample: nn.Module | None = None) -> None:
+    def __init__(
+        self,
+        inplanes: int,
+        planes: int,
+        stride: int = 1,
+        downsample: Optional[nn.Module] = None,
+        groups: int = 1,
+        base_width: int = 64,
+        dilation: int = 1,
+        norm_layer: Optional[Callable[..., nn.Module]] = None,
+    ) -> None:
         super().__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        if groups != 1 or base_width != 64:
+            raise ValueError("BasicBlock only supports groups=1 and base_width=64")
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
         self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = nn.BatchNorm2d(planes)
+        self.bn1 = norm_layer(planes)
         self.relu = nn.ReLU(inplace=True)
         self.conv2 = conv3x3(planes, planes)
-        self.bn2 = nn.BatchNorm2d(planes)
+        self.bn2 = norm_layer(planes)
         self.downsample = downsample
         self.stride = stride
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        residual = x
+    def forward(self, x: Tensor) -> Tensor:
+        identity = x
 
         out = self.conv1(x)
         out = self.bn1(out)
@@ -57,84 +62,173 @@ class BasicBlock(nn.Module):
         out = self.bn2(out)
 
         if self.downsample is not None:
-            residual = self.downsample(x)
+            identity = self.downsample(x)
 
-        out += residual
+        out += identity
         out = self.relu(out)
 
         return out  # type: ignore[no-any-return]
+    
+class Bottleneck(nn.Module):
+    # Bottleneck in torchvision places the stride for downsampling at 3x3 convolution(self.conv2)
+    # while original implementation places the stride at the first 1x1 convolution(self.conv1)
+    # according to "Deep residual learning for image recognition" https://arxiv.org/abs/1512.03385.
+    # This variant is also known as ResNet V1.5 and improves accuracy according to
+    # https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch.
 
+    expansion: int = 4
 
+    def __init__(
+        self,
+        inplanes: int,
+        planes: int,
+        stride: int = 1,
+        downsample: Optional[nn.Module] = None,
+        groups: int = 1,
+        base_width: int = 64,
+        dilation: int = 1,
+        norm_layer: Optional[Callable[..., nn.Module]] = None,
+    ) -> None:
+        super().__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        width = int(planes * (base_width / 64.0)) * groups
+        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv1x1(inplanes, width)
+        self.bn1 = norm_layer(width)
+        self.conv2 = conv3x3(width, width, stride, groups, dilation)
+        self.bn2 = norm_layer(width)
+        self.conv3 = conv1x1(width, planes * self.expansion)
+        self.bn3 = norm_layer(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x: Tensor) -> Tensor:
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out  # type: ignore[no-any-return]
+    
 class ResNet(nn.Module):
     def __init__(
-            self,
-            block: type[BasicBlock],
-            layers: list[int],
-            num_classes: int = 10) -> None:
-        self.inplanes = 64
+        self,
+        block: type[Union[BasicBlock, Bottleneck]],
+        layers: list[int],
+        num_classes: int = 1000,
+        zero_init_residual: bool = False,
+        groups: int = 1,
+        width_per_group: int = 64,
+        replace_stride_with_dilation: Optional[list[bool]] = None,
+        norm_layer: Optional[Callable[..., nn.Module]] = None,
+    ) -> None:
         super().__init__()
-        self.conv1 = nn.Conv2d(
-            3,
-            64,
-            kernel_size=7,
-            stride=2,
-            padding=3,
-            bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
+
+        self.inplanes = 64
+        self.dilation = 1
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError(
+                "replace_stride_with_dilation should be None "
+                f"or a 3-element tuple, got {replace_stride_with_dilation}"
+            )
+        self.groups = groups
+        self.base_width = width_per_group
+        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = norm_layer(self.inplanes)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, dilate=replace_stride_with_dilation[0])
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2, dilate=replace_stride_with_dilation[1])
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2, dilate=replace_stride_with_dilation[2])
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(512 * block.expansion, num_classes)
-        self._initialize_weights()
 
-    def _initialize_weights(self) -> None:
-        for module in self.modules():
-            if isinstance(module, nn.Conv2d):
-                nn.init.kaiming_normal_(
-                    module.weight, mode="fan_out", nonlinearity="relu")
-                if module.bias is not None:
-                    nn.init.zeros_(module.bias)
-            elif isinstance(module, (nn.BatchNorm2d, nn.GroupNorm)):
-                nn.init.ones_(module.weight)
-                nn.init.zeros_(module.bias)
-            elif isinstance(module, nn.Linear):
-                nn.init.normal_(module.weight, 0, 0.01)
-                nn.init.zeros_(module.bias)
+        self.initialize_weights(zero_init_residual)
+        
+    def initialize_weights(self, zero_init_residual: bool) -> None:
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
 
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck) and m.bn3.weight is not None:
+                    nn.init.constant_(m.bn3.weight, 0)  # type: ignore[arg-type]
+                elif isinstance(m, BasicBlock) and m.bn2.weight is not None:
+                    nn.init.constant_(m.bn2.weight, 0)  # type: ignore[arg-type]
+        
     def _make_layer(
-            self,
-            block: type[BasicBlock],
-            planes: int,
-            blocks: int,
-            stride: int = 1) -> nn.Sequential:
+        self,
+        block: type[Union[BasicBlock, Bottleneck]],
+        planes: int,
+        blocks: int,
+        stride: int = 1,
+        dilate: bool = False,
+    ) -> nn.Sequential:
+        norm_layer = self._norm_layer
         downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
-                nn.Conv2d(
-                    self.inplanes,
-                    planes *
-                    block.expansion,
-                    kernel_size=1,
-                    stride=stride,
-                    bias=False),
-                nn.BatchNorm2d(
-                    planes *
-                    block.expansion),
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),
             )
 
         layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
+        layers.append(
+            block(
+                self.inplanes, planes, stride, downsample, self.groups, self.base_width, previous_dilation, norm_layer
+            )
+        )
         self.inplanes = planes * block.expansion
-        for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
+        for _ in range(1, blocks):
+            layers.append(
+                block(
+                    self.inplanes,
+                    planes,
+                    groups=self.groups,
+                    base_width=self.base_width,
+                    dilation=self.dilation,
+                    norm_layer=norm_layer,
+                )
+            )
 
         return nn.Sequential(*layers)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    # Support torch.script function
+    def _forward_impl(self, x: Tensor) -> Tensor:
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
@@ -150,3 +244,112 @@ class ResNet(nn.Module):
         x = self.fc(x)
 
         return x
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self._forward_impl(x)
+from torchvision.models._api import WeightsEnum  # type: ignore[import-untyped]
+    
+class ResNet18(ResNet):
+    def __init__(self, num_classes: int = 1000, pretrained: bool = False) -> None:
+        super().__init__(block=BasicBlock, layers=[2, 2, 2, 2], num_classes=num_classes)
+        if pretrained:
+            self._pretrained()
+            if num_classes != 1000:
+                in_features = self.fc.in_features
+                self.fc = nn.Linear(in_features, num_classes)
+    def _pretrained(self) -> None:
+        state_dict = nn.Module.state_dict(self)
+        pretrained_dict = torch.hub.load_state_dict_from_url(
+            "https://download.pytorch.org/models/resnet18-f37072fd.pth",
+            progress=True,
+        )
+        # Filter out unnecessary keys
+        pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in state_dict}
+        # Overwrite entries in the existing state dict
+        state_dict.update(pretrained_dict)
+        # Load the new state dict
+        nn.Module.load_state_dict(self, state_dict)
+
+class ResNet34(ResNet):
+    def __init__(self, num_classes: int = 1000, pretrained: bool = False) -> None:
+        super().__init__(block=BasicBlock, layers=[3, 4, 6, 3], num_classes=num_classes)
+        if pretrained:
+            self._pretrained()
+            if num_classes != 1000:
+                in_features = self.fc.in_features
+                self.fc = nn.Linear(in_features, num_classes)
+    def _pretrained(self) -> None:
+        state_dict = nn.Module.state_dict(self)
+        pretrained_dict = torch.hub.load_state_dict_from_url(
+            "https://download.pytorch.org/models/resnet34-b627a593.pth",
+            progress=True,
+        )
+        # Filter out unnecessary keys
+        pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in state_dict}
+        # Overwrite entries in the existing state dict
+        state_dict.update(pretrained_dict)
+        # Load the new state dict
+        nn.Module.load_state_dict(self, state_dict)
+
+class ResNet50(ResNet):
+    def __init__(self, num_classes: int = 1000, pretrained: bool = False) -> None:
+        super().__init__(block=Bottleneck, layers=[3, 4, 6, 3], num_classes=num_classes)
+        if pretrained:
+            self._pretrained()
+            if num_classes != 1000:
+                in_features = self.fc.in_features
+                self.fc = nn.Linear(in_features, num_classes)
+    def _pretrained(self) -> None:
+        state_dict = nn.Module.state_dict(self)
+        pretrained_dict = torch.hub.load_state_dict_from_url(
+            "https://download.pytorch.org/models/resnet50-0676ba61.pth",
+            progress=True,
+        )
+        # Filter out unnecessary keys
+        pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in state_dict}
+        # Overwrite entries in the existing state dict
+        state_dict.update(pretrained_dict)
+        # Load the new state dict
+        nn.Module.load_state_dict(self, state_dict)
+        
+class ResNet101(ResNet):
+    def __init__(self, num_classes: int = 1000, pretrained: bool = False) -> None:
+        super().__init__(block=Bottleneck, layers=[3, 4, 23, 3], num_classes=num_classes)
+        if pretrained:
+            self._pretrained()
+            if num_classes != 1000:
+                in_features = self.fc.in_features
+                self.fc = nn.Linear(in_features, num_classes)
+    def _pretrained(self) -> None:
+        state_dict = nn.Module.state_dict(self)
+        pretrained_dict = torch.hub.load_state_dict_from_url(
+            "https://download.pytorch.org/models/resnet101-63fe2227.pth",
+            progress=True,
+        )
+        # Filter out unnecessary keys
+        pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in state_dict}
+        # Overwrite entries in the existing state dict
+        state_dict.update(pretrained_dict)
+        # Load the new state dict
+        nn.Module.load_state_dict(self, state_dict)
+
+class ResNet152(ResNet):
+    def __init__(self, num_classes: int = 1000, pretrained: bool = False) -> None:
+        super().__init__(block=Bottleneck, layers=[3, 8, 36, 3], num_classes=num_classes)
+        if pretrained:
+            self._pretrained()
+            if num_classes != 1000:
+                in_features = self.fc.in_features
+                self.fc = nn.Linear(in_features, num_classes)
+    def _pretrained(self) -> None:
+        state_dict = nn.Module.state_dict(self)
+        pretrained_dict = torch.hub.load_state_dict_from_url(
+            "https://download.pytorch.org/models/resnet152-b121ed2d.pth",
+            progress=True,
+        )
+        # Filter out unnecessary keys
+        pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in state_dict}
+        # Overwrite entries in the existing state dict
+        state_dict.update(pretrained_dict)
+        # Load the new state dict
+        nn.Module.load_state_dict(self, state_dict)
